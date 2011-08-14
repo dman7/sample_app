a3d84e79cf1205a9a100adeaa98a64619ad25519
Dmitri Skjorshammer <dmitriskj@gmail.com>
2011-08-14 05:53:25 -0700

    Finished User Model (Chapter 7)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
-gem 'sqlite3', '1.3.3'
+gem 'sqlite3', '1.3.3', :group => :development
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
 	gem 'rspec-rails','2.6.1'
@@ -13,6 +14,7 @@ group :test do
 	gem 'webrat', '0.7.1'
 	#gem 'autotest', '4.4.6'
 	#gem 'autotest-rails-pure', '4.1.2'
+	gem 'factory_girl_rails', '1.0'
 end
 
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,43 @@
 require 'spec_helper'
 
 describe UsersController do
-	render_views
+  render_views
+
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get(:show, {:id => @user})
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get(:show, {:id => @user}) #Alternatively, can use get('show',{:id=>@user}); Equivalently, :id=>@user.id
+      assigns(:user).should == @user
+    end
+
+    it "should have the right title" do
+      get(:show, {:id => @user})
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get(:show, {:id => @user})
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get(:show, {:id => @user})
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+
 
   describe "GET 'new'" do
-    it "should be successful" do
+  	it "should be successful" do
       get 'new'
       response.should be_success
     end
@@ -15,3 +48,6 @@ describe UsersController do
 		end
   end
 end
+
+
+
