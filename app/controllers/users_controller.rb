feb6e2b421cae7c98aae0e1fd2fa9ac76f69387a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2011-08-28 17:25:06 -0700

    Finished Chapter 10.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_filter :authenticate, :only => [:edit, :update] #before_filter(x, :only => Y) applies action x on all actions in set Y
+	before_filter :authenticate, :only => [:index, :edit, :update, :destroy] #before_filter(x, :only => Y) applies action x on all actions in set Y
 	before_filter :correct_user, :only => [:edit, :update]
+	before_filter :admin_user, :only => :destroy
 
   def new
 		@user = User.new
@@ -40,6 +41,17 @@ class UsersController < ApplicationController
     end
 	end
 
+	def index
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
+
+	def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+	end
+
 	private
 	def authenticate
 		deny_access unless signed_in?
@@ -48,5 +60,9 @@ class UsersController < ApplicationController
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to(root_path) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 end
